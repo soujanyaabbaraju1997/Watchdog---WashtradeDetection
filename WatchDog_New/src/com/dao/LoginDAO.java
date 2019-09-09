3a43cee4fdb282531476ce2903b2a3e3dbce12d0
miriyalamanisha <miriyalamanisha98@gmail.com>
2019-09-09 10:25:46 +0530

    login basic

---
+++ WatchDog_New/src/com/dao/LoginDAO.java
@@ -0,0 +1,45 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAO {
+
+	private Connection openConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("DRIVER LOADED SUCCESSFULLY");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "admin");
+			System.out.println("CONNECTION OBTAINED");
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e);
+		}
+
+		return conn;
+	}
+
+	public String dologin(String username, String password) {
+		String name = "no_user";
+		String SQL_LOGIN = "select username,password from users where " + "username=? and password=?";
+		try (Connection connection = openConnection(); PreparedStatement ps = connection.prepareStatement(SQL_LOGIN);) {
+
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ResultSet set = ps.executeQuery();
+			while (set.next()) {
+				name = set.getString(1);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return name;
+
+	}
+
+}
---
+++ WatchDog_New/src/com/servlets/LoginServlet.java
@@ -0,0 +1,96 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.LoginDAO;
+import com.dao.UserDAO;
+import com.dao.UserDAOImpl;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		
+		UserDAO userdao = new UserDAOImpl();
+		
+		int isadmin=userdao.findUserByUsername(username).isAdmin();
+		
+		
+		LoginDAO dao = new LoginDAO();
+		
+		String name=dao.dologin(username,password);
+		System.out.println("username "+name);
+		
+		if(name.equals("no_user"))
+		{
+			request.setAttribute("msg", "Credentials do not match.Try again");
+			
+			RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
+			
+		}
+		else
+		{
+			HttpSession httpsession =request.getSession();
+			System.out.println(httpsession.getId()+": "
+					+httpsession.getCreationTime());
+			httpsession.setAttribute("username", name);		
+			if(isadmin==1)
+			{
+				RequestDispatcher rd=request.getRequestDispatcher("admin.html");
+				rd.forward(request, response);
+
+				
+			}
+			else
+			{
+				RequestDispatcher rd=request.getRequestDispatcher("trader.html");
+				rd.forward(request, response);
+
+			}
+			
+			
+		}
+		//return name;
+	}
+
+		
+	
+
+}
