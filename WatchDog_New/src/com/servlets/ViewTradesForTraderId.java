c9f51a6ab1d7a131fb7ea089ec8cb6f1749f2f6c
miriyalamanisha <miriyalamanisha98@gmail.com>
2019-09-12 15:23:52 +0530

    viewtrades for traderid servlet

--- WatchDog_New/src/com/servlets/ViewTradesForTraderId.java
+++ WatchDog_New/src/com/servlets/ViewTradesForTraderId.java
@@ -1,11 +1,23 @@
 package com.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.TradeDAO;
+import com.dao.TradeDAOImpl;
+import com.dao.TraderDAO;
+import com.dao.TraderDAOImpl;
+import com.pojos.Trade;
+import com.pojos.Trader;
 
 /**
  * Servlet implementation class ViewTradesForTraderId
@@ -35,7 +47,26 @@ public class ViewTradesForTraderId extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		HttpSession httpsession =request.getSession();
+		String name;
+		name=(String) httpsession.getAttribute("username");
+		TraderDAOImpl tdao = new TraderDAOImpl();
+		TradeDAOImpl tradedao = new TradeDAOImpl();
+		Trader t = new Trader();
+		t=tdao.findByUsername(name);
+		List <Trade> tradelist= new ArrayList<Trade>();
+		tradelist=tradedao.findByTraderId(t.getTraderId());
+		
+		
+		request.setAttribute("data",tradelist);
+		
+		//CHANGE THE NAME OF THE TARGET PAGE AND DELETE COMMENT
+		RequestDispatcher dispatcher = request.getRequestDispatcher("page.html");
+		dispatcher.forward(request, response);
+
+		
+		//doGet(request, response);
 	}
 
 }
