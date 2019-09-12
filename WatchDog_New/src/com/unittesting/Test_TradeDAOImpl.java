bd3021b1c4773fd251740db045d8566f90c405e5
rupashi97 <rupsangal@gmail.com>
2019-09-12 12:42:01 +0530

    test trade_dao_impl

--- WatchDog_New/src/com/unittesting/Test_TradeDAOImpl.java
+++ WatchDog_New/src/com/unittesting/Test_TradeDAOImpl.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.dao.TradeDAOImpl;
+import com.dao.TraderDAOImpl;
 import com.pojos.Trade;
 import com.pojos.Trader;
 
@@ -25,53 +26,75 @@ class Test_TradeDAOImpl {
 	void tearDown() throws Exception {
 	}
 
-//	@Test
-//	void testAddTrade() {
-//		//fail("Not yet implemented");
+	@Test
+	void testAddTrade() {
+		//fail("Not yet implemented");
 //		TradeDAOImpl dao= new TradeDAOImpl();
-//		Trader trader= new Trader();
-//		Time time= System.currentTimeMillis();
+//		TraderDAOImpl dao1 = new TraderDAOImpl();
+//		
+//		Trader trader= dao1.findByTraderID("HARMENON");
 //		Timestamp time= new Timestamp(System.currentTimeMillis());
-//		Trade trade = new Trade(345634, trader,time,"nonsense",1204,789,567.78f,101,"Zerodha",0);
+//		
+//		//Positive test case
+//		
+//		Trade trade = new Trade(345634, trader,time,"nonsense",1204,789,567.78f,101,"ZERODHA",0);
 //		int actual=dao.addTrade(trade);
 //		assertEquals(1, actual);
 //		
-//	}
+//		//Negative test case
+//		Trade trade1 = new Trade(100138, trader,time,"nonsense",1204,789,567.78f,101,"ZERODHA",0);
+//		int actual1=dao.addTrade(trade1);
+//		assertEquals(0, actual1);
+		
+	}
 
 //	@Test
-//	void testFindTradeById() {
-//		//fail("Not yet implemented");
+	void testFindTradeById() {
+		//fail("Not yet implemented");
+		
 //		TradeDAOImpl dao= new TradeDAOImpl();
+//		
+//		//Positive test case
 //		Trade trade = new Trade();
 //		trade=dao.findTradeById(100405);
 //		assertEquals(100405, trade.getTradeId());
 //		
-//		
-//	}
-//
-//	@Test
-//	void testFindAllTrades() {
-//		//fail("Not yet implemented");
+//		//Negative test case
+//		Trade trade1 = new Trade();
+//		trade1=dao.findTradeById(333333);
+//		assertEquals(0, trade1.getTradeId());
+		
+		
+	}
+
+	@Test
+	void testFindAllTrades() {
+		//fail("Not yet implemented");
+		
 //		TradeDAOImpl dao= new TradeDAOImpl();
 //		List<Trade> tradelist = new ArrayList<Trade>();
 //		tradelist=dao.findAllTrades();
+//		
 //		System.out.println(tradelist.size());
-//		assertEquals(800, tradelist.size());
-//	}
+//		assertEquals(43, tradelist.size());
+	}
 	
-//
-//	@Test
-//	void testFilterTraders() {
-//		fail("Not yet implemented");
-//	}
-//
 	@Test
 	void testFindByTraderId() {
 		//fail("Not yet implemented");
-	TradeDAOImpl dao= new TradeDAOImpl();
-	List<Trade> tradelist = new ArrayList<Trade>();
-	tradelist=dao.findByTraderId("GAU7P0MO");
-	assertEquals(8, tradelist.size());
+//	TradeDAOImpl dao= new TradeDAOImpl();
+//		
+//	//Positive test case	
+//	List<Trade> tradelist = new ArrayList<Trade>();
+//	tradelist=dao.findByTraderId("HARMENON");
+//	assertEquals(9, tradelist.size());
+//	
+//	
+//	//Negative test case
+//	List<Trade> tradelist1 = new ArrayList<Trade>();
+//	tradelist1=dao.findByTraderId("ABCMENON");
+//	assertEquals(0, tradelist1.size());
+	
 	}
 
 	
